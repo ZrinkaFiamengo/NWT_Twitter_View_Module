a83e0b0585ecd72354b8493782ca51999b7fd72c
Zrinka Fiamengo <zfiame00@fesb.hr>
2016-01-02 15:24:55 +0100

    Adding new Tweet

--- NWT_test/Scripts/App/Components/NewTweet.ts
+++ NWT_test/Scripts/App/Components/NewTweet.ts
@@ -1,8 +1,8 @@
-﻿import {Component, View, NgFor} from "angular2/angular2"
-import {Tweet as TweetModel} from "./../Model/Tweet"
+﻿import {Component, View, EventEmitter} from "angular2/angular2"
 
 @Component({
-    selector: "new-tweet"
+    selector: "new-tweet",
+    outputs: ["publish"]
 })
 
 @View({
@@ -12,7 +12,7 @@ import {Tweet as TweetModel} from "./../Model/Tweet"
                     <img src="/Content/Users/User1.png" class="user-image img-from-new-tweet-window" alt="user picture" />
                     <input class="form-control input-from-new-tweet-window" placeholder="What's happening?" #input (keyup)="newTweetUpdate(input)">
                 </div>
-                <button class="btn btn-success pull-right" type="button">Post</button>
+                <button class="btn btn-success pull-right" type="button" (click)="onPublish(input)">Post</button>
                 <span class="pull-right number-from-new-tweet-window">{{numberOfSymbols}}</span>
                 <ul class="list-inline new-tweet-list">
                     <li><a href="#"><i class="glyphicon glyphicon-facetime-video"></i>  Media</a></li>
@@ -26,13 +26,35 @@ import {Tweet as TweetModel} from "./../Model/Tweet"
 
 export class NewTweet {
     private numberOfSymbols: number;
+    public publish: EventEmitter;
 
     constructor() {
         this.numberOfSymbols = 140;
+        this.publish = new EventEmitter();
     }
 
     private newTweetUpdate(input: HTMLInputElement): void {
         this.numberOfSymbols = 140 - input.value.length;
+        if (input.value.length > 140)
+            input.setAttribute("style", "background-color: #FFCCCC;");
+        else if (input.value.length > 130)
+            input.setAttribute("style", "background-color: #FFFCCC;");
+        else
+            input.setAttribute("style", "background-color: none;");
+    }
+
+    private onPublish(input: HTMLInputElement): void {
+        var value = input.value.trim();
+        if (value == "") { return; }
+
+        if (value.length > 140) {
+            return;
+        }
+
+        this.publish.next(value);
+
+        input.value = "";
+        this.numberOfSymbols = 140;
     }
 }
 
