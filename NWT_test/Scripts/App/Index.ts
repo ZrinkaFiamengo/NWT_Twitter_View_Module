e5c463119bb047f082a9edd9b3db93321516a4cb
Zrinka Fiamengo <zfiame00@fesb.hr>
2016-01-03 16:44:36 +0100

    Added Index tracking

--- NWT_test/Scripts/App/Index.ts
+++ NWT_test/Scripts/App/Index.ts
@@ -5,34 +5,32 @@ import {User as UserModel} from "./Model/User"
 import {TweetsList} from "./Components/TweetsList"
 import {Trends} from "./Components/Trends"
 import {UserInfo} from "./Components/UserInfo"
-import {ProfileBox} from "./Components/ProfileBox"
+import {NewTweet} from "./Components/NewTweet"
 
 
 @Component({
-    selector: "profile"
+    selector: "index"
 })
 
-@View({
-        directives: [TweetsList, Trends, UserInfo, ProfileBox],
+    @View({
+        directives: [TweetsList, Trends, UserInfo, NewTweet],
     template:
-    `
-    <profile-box [user] = "currentUser" >Loading user info..</profile-box>
+    `<main>
 
-<main>
-    <div id="profile-data-div">
-        <tweets-list [tweets]="currentUser.tweets" [current-user]="currentUser" (put-favourited)="onPutFavourited($event)"  (remove-favourited)="onRemoveFavourited($event)">Loading User's Tweets..</tweets-list>
+    <aside id="user-panel-container">
+        <user-info [user] = "currentUser" >Loading user info..</user-info>
+        <trends [hashtags]="hashtags" >Loading list..</trends>
+    </aside>
+
+    <div id="main-content-container" class="panel">
+        <new-tweet (publish)="onNewTweetPublish($event)">Loading new tweet window..</new-tweet>
+        <tweets-list [tweets]="tweets" [current-user]="currentUser" (put-favourited)="onPutFavourited($event)"  (remove-favourited)="onRemoveFavourited($event)">Loading Users Tweets..</tweets-list>
     </div>
 
-    <aside id="user-panel-container" class="hidden-xs trends">
-
-        <trends [hashtags]="hashtags" >Loading list..</trends>
-
-    </aside>
-</main>
-`
+</main>`
 })
 
-export class Profile {
+export class Index {
     public hashtags: HashtagModel[];
     public tweets: TweetModel[];
     public users: UserModel[];
@@ -60,17 +58,24 @@ export class Profile {
         this.users[2].following = [this.users[3], this.users[1], this.users[0]];
         this.users[3].following = [this.users[2], this.users[1]];
 
+        this.currentUser = this.users[0];
+
         this.tweets = [
             new TweetModel(this.users[0], new Date(2015, 12, 12), "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", [this.hashtags[0], this.hashtags[4]]),
-            new TweetModel(this.users[1], new Date(2015, 12, 10), "Duis malesuada leo justo, eget efficitur ligula varius sed. Suspendisse potenti. Integer imperdiet lobortis leo. Integer ullamcorper iaculis convallis.", [this.hashtags[5], this.hashtags[1], this.hashtags[3]]),
+            new TweetModel(this.users[1],new Date(2015, 12, 10), "Duis malesuada leo justo, eget efficitur ligula varius sed. Suspendisse potenti. Integer imperdiet lobortis leo. Integer ullamcorper iaculis convallis.", [this.hashtags[5], this.hashtags[1], this.hashtags[3]]),
             new TweetModel(this.users[2], new Date(2015, 12, 4), "Aliquam quam quam, dignissim non eros nec, congue scelerisque arcu. Nam ac nibh massa. Suspendisse tristique ante vel ultricies congue. Mauris sagittis nec tortor vitae rutrum.", [this.hashtags[5]]),
             new TweetModel(this.users[0], new Date(2015, 12, 2), "Donec lacinia massa lectus. Sed a tristique odio.", [this.hashtags[3], this.hashtags[4], this.hashtags[1]])
         ];
 
-        this.currentUser = this.users[0];
-
         this.currentUser.tweets = this.tweets.filter(tweet => tweet.author == this.currentUser);
+        this.currentUser.favourites = [this.tweets[1], this.tweets[2]];
     }
+
+    private onNewTweetPublish(data: string) {
+
+        this.tweets.unshift(new TweetModel(this.currentUser, new Date(), data));
+    }
+
 
     private onPutFavourited(favourite: TweetModel): void {
         this.currentUser.favourites.push(favourite);
@@ -84,4 +89,4 @@ export class Profile {
     }
 }
 
-bootstrap(Profile);
+bootstrap(Index);
